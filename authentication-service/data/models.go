6a5987113a49f5e9dd9542c6827255f03046fa72
ilhamKawe <kawekaweha123@gmail.com>
2023-12-19 16:48:27 +0700

    set authentication-service main and routes

---
+++ authentication-service/data/models.go
@@ -0,0 +1,33 @@
+package data
+
+import (
+	"database/sql"
+	"time"
+)
+
+const dbTimeout = time.Second * 3
+
+var db *sql.DB
+
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User: User{},
+	}
+}
+
+type Models struct {
+	User User
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	Email     string    `json:"email"`
+	FirstName string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"-"`
+	Active    int       `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
